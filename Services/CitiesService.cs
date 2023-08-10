9668a44922bc3dd1473499a50524376e8f36a527
Vishal <vishalbhat.com@gmail.com>
2023-08-10 12:24:05 +0530

    Add project files.

---
+++ Services/CitiesService.cs
@@ -0,0 +1,25 @@
+﻿using ServiceContracts;
+
+namespace Services
+{
+  public class CitiesService : ICitiesService
+  {
+    private List<string> _cities;
+
+    public CitiesService()
+    {
+      _cities = new List<string>() { 
+        "Bangalore",
+        "kumta",
+        "New Delhi",
+        "Pune",
+        "Mumbai"
+      };
+    }
+
+    public List<string> GetCities()
+    {
+      return _cities;
+    }
+  }
+}
